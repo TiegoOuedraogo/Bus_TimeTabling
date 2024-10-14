b382ae494fd2a1c773b3ce94f30933c3ca91413a
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-14 11:03:06 -0400

    route controller + repo

--- src/main/java/com/example/bus_timetabling/controller/RouteController.java
+++ src/main/java/com/example/bus_timetabling/controller/RouteController.java
@@ -1,4 +1,11 @@
 package com.example.bus_timetabling.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/route")
+@CrossOrigin
 public class RouteController {
 }

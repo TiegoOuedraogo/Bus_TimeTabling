09c63a4f3486b4c25b5f5edc4665ed3e7e2404cd
Kais <kais.sheikh@peopleshores.com>
2024-10-14 12:01:33 -0400

    fixing DBeaver compatibility

--- src/main/java/com/example/bus_timetabling/repository/BusRepository.java
+++ src/main/java/com/example/bus_timetabling/repository/BusRepository.java
@@ -3,6 +3,6 @@ package com.example.bus_timetabling.repository;
 import com.example.bus_timetabling.entities.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BusRepository extends JpaRepository<Bus, Integer> {
+public interface BusRepository extends JpaRepository<Bus, Long> {
 
 }

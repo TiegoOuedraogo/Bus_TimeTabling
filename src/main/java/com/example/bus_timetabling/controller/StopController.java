a2b244503ae5c5c5d745428395a6248955acd745
mrfritzg <mrfritz@gamil.com>
2024-10-14 12:48:21 -0400

    1st commit to add stop repo, controller and service

--- src/main/java/com/example/bus_timetabling/controller/StopController.java
+++ src/main/java/com/example/bus_timetabling/controller/StopController.java
@@ -1,4 +1,29 @@
 package com.example.bus_timetabling.controller;
 
+import com.example.bus_timetabling.entities.Stop;
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/stops")
 public class StopController {
+
+    @GetMapping("")
+    //turn into DTO object response later
+    public String getStops() {
+        return "These are the stops";
+    }
+
+    @GetMapping("/{id}")
+    //turn into DTO object response later
+    public Long getStopDetail(@PathVariable Long id) {
+        return id;
+    }
+
+    @PostMapping(produces = "application/json")
+    public String createStop(@RequestBody Stop stop) {
+        return "Your stop has been created";
+    }
 }
--- src/main/java/com/example/bus_timetabling/repository/StopRepository.java
+++ src/main/java/com/example/bus_timetabling/repository/StopRepository.java
@@ -1,4 +1,9 @@
 package com.example.bus_timetabling.repository;
 
-public interface StopRepository {
+import com.example.bus_timetabling.entities.Stop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StopRepository extends JpaRepository<Stop, Integer> {
 }

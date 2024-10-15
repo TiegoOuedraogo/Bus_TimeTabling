cb1d59b77c0c10eedf1d21ab3dd4042d5a5af3ac
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-15 10:39:09 -0400

    mappers with Lombork, controller skeleton, service interface

--- src/main/java/com/example/bus_timetabling/service/RouteService.java
+++ src/main/java/com/example/bus_timetabling/service/RouteService.java
@@ -5,7 +5,7 @@ import com.example.bus_timetabling.dto.RouteResponseDto;
 import com.example.bus_timetabling.exception.RouteNotFoundException;
 
 import java.util.List;
-
+//
 public interface RouteService<Route ,Long> {
   RouteResponseDto createRoute(RouteRequestDto routeRequestDto);
   List<RouteResponseDto> retrieveAllRoutes ();

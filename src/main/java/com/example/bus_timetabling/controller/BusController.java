fc162631b9824021c649798fdbd2066b74862b96
Kais <kais.sheikh@peopleshores.com>
2024-10-15 11:01:42 -0400

    Added a few services for bus entities and started making endpoints

--- src/main/java/com/example/bus_timetabling/controller/BusController.java
+++ src/main/java/com/example/bus_timetabling/controller/BusController.java
@@ -1,10 +1,14 @@
 package com.example.bus_timetabling.controller;
 
-import com.example.bus_timetabling.dto.BusDto;
+import com.example.bus_timetabling.dto.BusResponseDto;
+import com.example.bus_timetabling.entities.Bus;
 import com.example.bus_timetabling.service.BusService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BusController {
@@ -15,8 +19,30 @@ public class BusController {
         this.busService = busService;
     }
 
-//    @PostMapping("/{bus-id}")
-//    public BusDto findBusById(@PathVariable("student-id") Integer id) {
-//        return busService.findBusById(id);
-//    }
+    @GetMapping("/bus/{id}")
+    public BusResponseDto findBusById(@PathVariable("student-id") Long id) {
+        return busService.findBusById(id);
+    }
+
+    @GetMapping("/bus/{busNumber}")
+    public List<BusResponseDto> findBusByNumber(@PathVariable("busNumber") String busNumber) {
+        return busService.findBusByNumber(busNumber);
+    }
+
+    @GetMapping("/bus")
+    public List<BusResponseDto> getAllBuses() {
+        return busService.getAllBuses();
+    }
+
+    @GetMapping("/bus/{routeId}")
+    public List<BusResponseDto> findBusByRouteId(@PathVariable("routeId") Long routeId) {
+        return busService.findBusByRouteId(routeId);
+    }
+
+    @GetMapping("/bus/{stopId}")
+    public List<BusResponseDto> findBusByStopId(@PathVariable("stopId") Long stopId) {
+        return busService.findBusByStopId(stopId);
+    }
+
+
 }
--- src/main/java/com/example/bus_timetabling/repository/BusRepository.java
+++ src/main/java/com/example/bus_timetabling/repository/BusRepository.java
@@ -3,6 +3,12 @@ package com.example.bus_timetabling.repository;
 import com.example.bus_timetabling.entities.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BusRepository extends JpaRepository<Bus, Long> {
+import java.util.List;
 
+public interface BusRepository extends JpaRepository<Bus, Long> {
+    List<Bus> findByBusNumber(String busNumber);
+
+    List<Bus> findBusByRouteId(Long Id);
+
+    List<Bus> findBusByStopId(Long Id);
 }

ee64e61916fe55c58e55dbcd3dcfb44aea70a048
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-16 17:15:36 -0400

    mappers

--- src/main/java/com/example/bus_timetabling/service/RouteServiceImplementation.java
+++ src/main/java/com/example/bus_timetabling/service/RouteServiceImplementation.java
@@ -3,12 +3,19 @@ package com.example.bus_timetabling.service;
 import com.example.bus_timetabling.dto.RouteRequestDto;
 import com.example.bus_timetabling.dto.RouteResponseDto;
 import com.example.bus_timetabling.exception.RouteNotFoundException;
+import com.example.bus_timetabling.repository.RouteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RouteServiceImplementation implements RouteService {
+    private final RouteRepository routeRepository;
+
+    public RouteServiceImplementation(RouteRepository routeRepository) {
+        this.routeRepository = routeRepository;
+    }
+
     @Override
     public RouteResponseDto createRoute(RouteRequestDto routeRequestDto) {
         return null;

21174fe20e4ff8e75b108bd48dcc16858eb0412e
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-10-28 09:59:22 -0400

    save into Note.md

--- src/main/java/com/example/bus_timetabling/service/serviceImpl/RouteStopScheduleServiceImpl.java
+++ src/main/java/com/example/bus_timetabling/service/serviceImpl/RouteStopScheduleServiceImpl.java
@@ -36,3 +36,4 @@ public class RouteStopScheduleServiceImpl implements RouteStopScheduleService {
     }
 
 }
+
--- src/main/java/com/example/bus_timetabling/service/serviceImpl/TimesTableServiceImpl.java
+++ src/main/java/com/example/bus_timetabling/service/serviceImpl/TimesTableServiceImpl.java
@@ -62,3 +62,4 @@ public class TimesTableServiceImpl implements TimeTableService {
                .build();
     }
 }
+
